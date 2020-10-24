8411100545ef5ae90934c40c3dd8efb13d0c509a
Arica Jackson <aricajackson2020@gmail.com>
2020-10-23 22:00:31 -0400

    Updating routes

--- app.py
+++ app.py
@@ -3,9 +3,55 @@ import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, inspect, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 from flask import Flask, jsonify
 
 #Database Setup
 engine = create_engine("(Resources/hawaii.sqlite)")
+
+#reflect database
+Base = automap_base()
+#reflect tables
+Base.prepare(engine, reflect=True)
+Station = Base.classes.station
+Measurement = Base.classes.measurement
+
+#Flask Setup
+
+app = Flask(__name__)
+
+#Flask Routes
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return(
+        f"Welcome to the Climate App!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+        
+
+    )
+climate_app_prcp = [
+    {"date": "prcp"}
+]
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the climate app precipitation data as json"""
+    
+    return jsonify(climate_app_prcp)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    """Return a list of all stations"""
+    results = session.query(stations.name).all()
+    
+
+
